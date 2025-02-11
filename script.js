6f6dc24bde7a8e51171a4979761ef69af0e844a3
Quy Nguyen <quythieu34@gmail.com>
2025-02-11 11:27:07 +0100

    picture animation

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const photo = document.getElementById('photo');
 const captureButton = document.getElementById('captureButton');
+const capturedphoto = document.querySelector('.captured-polaroid')
 
 async function startCamera() {
 	try {
@@ -46,6 +47,8 @@ captureButton.addEventListener('click', () => {
 	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 	const imageDataUrl = canvas.toDataURL('image/jpeg');
 	photo.src = imageDataUrl;
+	capturedphoto.classList.add("show");
+
 });
 
 window.addEventListener('load', startCamera);
