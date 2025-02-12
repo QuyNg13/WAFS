f451e6f6390ca38264f6071637548cfb889f96dd
Quy Nguyen <quythieu34@gmail.com>
2025-02-12 11:58:40 +0100

    content fill

--- script.js
+++ script.js
@@ -2,20 +2,38 @@ const baseURL = 'https://fdnd.directus.app/';
 const endpointMe = 'items/person/217'
 const myURL = baseURL + endpointMe;
 
-getData(myURL).then( data217 => {
-	let deH1 = document.querySelector("h1");
-	let myName = data217.data.name;
-	deH1.textContent = myName;
+getData(myURL).then(data217 => {
+    let deH1 = document.querySelector("h1");
+    let wbisection = document.getElementById("wbi");
+    let user = data217.data;
+    let myName = user.name;
+    let birthDate = user.birthdate;
+
+    let customData = JSON.parse(user.custom);
+
+    let age = customData.leeftijd;
+    let hometown = customData.woonplaats;
+
+    let wbiHTML = `<ul>
+        <li>Naam: ${myName}</li>
+        <li>Geboortedatum: ${birthDate}</li>
+        <li>Leeftijd: ${age}</li>
+        <li>Woonplaats: ${hometown}</li>
+    </ul>`;
+
+    wbisection.insertAdjacentHTML("beforeend", wbiHTML);
+    deH1.textContent = myName;
 });
+
 async function getData(URL) {
 	return (
-		fetch(URL) 
-		.then ( 
-			response => response.json() 
-		)
-		.then ( 
-			jsonData => {return jsonData}
-		)
+		fetch(URL)
+			.then(
+				response => response.json()
+			)
+			.then(
+				jsonData => { return jsonData }
+			)
 	);
 }
 
@@ -28,10 +46,10 @@ const lens = document.querySelector('.lens-glass');
 
 async function startCamera() {
 	try {
-		const stream = await navigator.mediaDevices.getUserMedia({ 
-			video: { 
-				facingMode: 'user' 
-			} 
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: {
+				facingMode: 'user'
+			}
 		});
 		video.srcObject = stream;
 	} catch (err) {
@@ -49,7 +67,7 @@ captureButton.addEventListener('click', () => {
 	lens.addEventListener('animationend', () => {
 		capturedphoto.classList.add("show");
 		canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-		const imageDataUrl = canvas.toDataURL('image/jpeg'); 
+		const imageDataUrl = canvas.toDataURL('image/jpeg');
 		photo.src = imageDataUrl;
 		photo.classList.add("show")
 	}, { once: true });
